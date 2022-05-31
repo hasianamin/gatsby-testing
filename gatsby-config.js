ecb9ed2c7722582a6429dea845d5fdf37493f3a5
hasianamin <hasianamin14@gmail.com>
2022-05-31 13:25:59 +0700

    add tailwind and axios

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", 'gatsby-plugin-postcss', {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
